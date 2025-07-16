c1449e73d6a971dc5a6a656ded23581c8573829d
FedericoPiazzolla <Federicop257@gmail.com>
2025-07-16 17:48:47 +0200

    add: useState in app.tsx and map COurseGoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
 import Header from "./components/Header.tsx";
 import CourseGoal from "./components/CourseGoal.tsx";
 import goalsImg from "./assets/goals.jpg";
 
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function addGoalHandler() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React +TS",
+        description: "Learn it from the ground up",
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your course goals</h1>
       </Header>
+      <button onClick={addGoalHandler}>Add Goal</button>
 
-      <CourseGoal title="Learn React + TS">
-        <p>Learn it from the groud up</p>
-      </CourseGoal>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
